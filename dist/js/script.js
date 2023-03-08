6d1df1d9fd45bb0da14c2ab20aa73c9e1d83aaaa
yuan levai leo <yuanlevaileo@MacBook-Air-yuan.local>
2023-03-08 14:23:59 +0700

    tambah fitur baru dan dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.add('flex');
+        toTop.classList.remove('hidden');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 };
 
@@ -19,3 +24,33 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// diluar hamburger
+
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// darkmode toogle
+const darkToogle = document.querySelector('#dark-toogle');
+const html = document.querySelector('html');
+
+darkToogle.addEventListener('click', function () {
+    if (darkToogle.checked) {
+        html.classList.add('dark');
+        localStorage.theme('dark');
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme('light');
+    }
+});
+
+// gesr toogle mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToogle.checked = true;
+} else {
+    darkToogle.checked = false;
+}
